3ddd90bfc4ec50529a491a37bb994f4810b4c1d3
baruwaa12 <baruwaademola@outlook.com>
2022-07-31 18:50:49 +0100

    Need to fix error 13 issue.

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,17 +1,23 @@
-from solcx import compile_standard, install_solc
-
 import json
 
+from web3 import Web3
+
+# In the video, we forget to `install_solc`
+# from solcx import compile_standard
+from solcx import compile_standard, install_solc
+import os
+
+with open(
+    "C:\\Users\\Baruw\\OneDrive\\New folder\\Documents\\Solidity\\web3_py_simple_storage"
+) as file:
+    simple_storage_file = file.read()
+
+# We add these two lines that we forgot from the video!
+print("Installing...")
 install_solc("0.6.0")
 
-with open("./web3_py_simple_storage/SimpleStorage.sol", "r") as file:
-    simple_storage_file = file.read()
-    print(simple_storage_file)
-
-
-# Compiled our solidity
-
-complied_sol = compile_standard(
+# Solidity source code
+compiled_sol = compile_standard(
     {
         "language": "Solidity",
         "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
@@ -27,13 +33,39 @@ complied_sol = compile_standard(
 )
 
 with open("compiled_code.json", "w") as file:
-    json.dump(complied_sol, file)
+    json.dump(compiled_sol, file)
 
-
-# Gets bytecode - (object code that an interpreter converts into binary machine code)
-bytecode = complied_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
     "bytecode"
 ]["object"]
 
-# Get the Abi - ()
-abi = complied_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+# get Abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+## To connect to ganache - use http link ion
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0xBcEcE348183a44d4Aa16567DfE522019F6910189"
+private_key = "0xae0a005b74fb53694206290f6f441a36476d04a16bef5286272062a3a49f69d2"
+
+
+# Create the contract in python.
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print(nonce)
+
+# Build, sign and send a transaction
+
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainID": chain_id, "nonce": nonce}
+)
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# Send this signed transaction to the blockchain
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
